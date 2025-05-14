3c6dfbe7b27d413db8c76f796e9ef44fed1b522a
tchernodawda <tchernodawda@gmail.com>
2025-05-14 11:33:47 +0000

    Modification de db.js

--- db.js
+++ db.js
@@ -1,14 +1,27 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-});
+// Utiliser l'URL de connexion fournie par Railway
+const connectionString = process.env.DATABASE_URL || process.env.POSTGRESQL_URL;
+
+// Si l'URL de connexion est disponible, l'utiliser
+const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: false 
+      } : false
+    })
+  : 
+    new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME
+    });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end()
 };
